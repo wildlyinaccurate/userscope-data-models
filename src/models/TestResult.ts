cf4714b61c558322e398420ffe6f39a31b4f2bd1
Joseph Wynn <joseph@wildlyinaccurate.com>
2019-11-17 18:18:08 +1300

    Add testingErrors to TestResult

--- src/models/TestResult.ts
+++ src/models/TestResult.ts
@@ -2,12 +2,14 @@ import { Document, Schema } from "mongoose"
 import { TeamDocument } from "./Team"
 import { BbcA11yResult, bbcA11yResultSchema } from "./BbcA11yResult"
 import { LighthouseResult, lighthouseResultSchema } from "./LighthouseResult"
+import { TestResultError } from "../types/TestResultError"
 
 export type TestResultDocument = Document & {
   url: string
   team: TeamDocument
   bbcA11yResults: BbcA11yResult
   lighthouseResults: LighthouseResult
+  testingErrors: [TestResultError]
 }
 
 export const testResultSchema = new Schema(
@@ -18,7 +20,8 @@ export const testResultSchema = new Schema(
       ref: "Team"
     },
     bbcA11yResults: bbcA11yResultSchema,
-    lighthouseResults: lighthouseResultSchema
+    lighthouseResults: lighthouseResultSchema,
+    testingErrors: [Schema.Types.Mixed]
   },
   { timestamps: true }
 )
---
+++ src/types/TestResultError.ts
@@ -0,0 +1,4 @@
+export type TestResultError = {
+  message: string
+  error: Error
+}
